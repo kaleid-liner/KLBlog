7291750e250e5994591d4a951e8f097aac851152
kaleid-liner <noob@mail.ustc.edu.cn>
2019-02-28 13:40:28 +0800

    update settings.py for deployment use

--- KLBlog/settings.py
+++ KLBlog/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # uncomment this line to deploy 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'), # comment this line to deploy
 ]
 
 LOGIN_REDIRECT_URL = '/'
