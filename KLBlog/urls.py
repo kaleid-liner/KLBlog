19b5ff84c064aa370e811ce7592d3fc64afc1d5d
kaleid-liner <32812933+kaleid-liner@users.noreply.github.com>
2019-03-05 20:53:55 +0800

    serve staticfiles development

--- KLBlog/urls.py
+++ KLBlog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django_registration.backends.one_step.views import RegistrationView
 from django.conf.urls.static import static
 from . import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +34,4 @@ urlpatterns = [
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
